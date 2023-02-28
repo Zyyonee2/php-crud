e8511df84958787c4e6394209cb60f3f5a3af7a5
Zyyonee2 <gimunhasanudin@gmail.com>
2023-02-28 11:10:19 +0700

    upload repo crud

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Selamat Datang di web crud contact php mysql</h2>
+	<p>Halaman Home Crud!</p>
+</div>
+
+<?=template_footer()?>
